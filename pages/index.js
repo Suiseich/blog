41a2a721183fad480f73d6246b37c22d7eac687b
Suiseich <aarontayag89@gmail.com>
2025-08-19 14:31:45 +0800

    Updated index.js

--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,9 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi, my name is Mickheil Aaron D. Tayag, and I am a student from the University of the Assumption. I've loved playing video games since I was a child, particularly MMORPGs and Souls games like Elden Ring. I also enjoy listening to video game OSTs and J-pop, with an artist like Hoshimachi Suisei being one of my favorites. My favorite food is lasagna. I see myself becoming an IT Support Specialist because it's a profession I'm truly passionate about. Having grown up loving computers, I really want to help people diagnose and fix problems with their desktops.</p>
+        <p>Hi, my name is Mickheil Aaron D. Tayag, and I am a student from the University of the Assumption. I've loved playing video games since I was a child, particularly MMORPGs and Souls games like Elden Ring. I also enjoy listening to video game OSTs and J-pop, with an artist like Hoshimachi Suisei being one of my favorites. My favorite food is lasagna.</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          I see myself becoming an IT Support Specialist because it's a profession I'm truly passionate about. Having grown up loving computers, I really want to help people diagnose and fix problems with their desktops.
         </p>
       </section>
     </Layout>
